7f323dd6d69ccb936dc8975fa091c6d17571d437
Katherine Gomez Alderete <katherinealdereteg@gmail.com>
2023-04-22 13:39:34 -0400

    modificaciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,26 @@ btnConsultar.addEventListener('click', function(){
         dataType: "json",
         success: function (response) {
             console.log(response);
-            crearGrafico(response); //es la funcion que estoy haciendo abajo
+            crearAgregarImagen(response); //es la funcion imagen que estoy ejecutando abajo
+            crearGrafico(response); //es la funcion grafico que estoy ejecutando abajo
             //si necesito más funciones con los mismos datos las voy sumando acá abajo
         }
     });
     txtNombrePokemon.value = ''; //para consultar la url
 });
+
+function crearAgregarImagen(response) {
+    //Paso 1: creo la caja donde poner la imagen y luego veo la imagen y el link
+    var contenedorImagen = document.querySelector('#contenedorImagen');
+    //Paso 3: creo imagen
+    var imagenEtiqueta = document.createElement('img');
+    //si linkeo al html una imagen debo poner ambos
+    imagenEtiqueta.setAttribute('src', response.sprites.front_default);
+    imagenEtiqueta.setAttribute('alt', 'Jugando para ver si funciona');
+    contenedorImagen.append(imagenEtiqueta);
+    //console.log(imagenEtiqueta);
+    
+}
 
 function crearGrafico(datos){
     var chart = new CanvasJS.Chart("graficoUno", {  // va al canvas y ejecuta el metodo chart
